9d0c340853da8754d1b01c76410ec654c040b930
Chris Whiting <mpro34@gmail.com>
2019-12-19 18:02:26 -0600

    Perform triangle movement by updating an offset value and updating the location of the triangle every iteration of the game loop

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxoffset = 0.7f;
+float triIncrement = 0.0005f;
 
 // Vertex Shader
 static const char* vShader = "										\n\
@@ -14,9 +20,11 @@ static const char* vShader = "										\n\
 																	\n\
 layout (location = 0) in vec3 pos;									\n\
 																	\n\
+uniform float xMove;												\n\
+																	\n\
 void main()															\n\
 {																	\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);       \n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);       \n\
 }";
 
 // Fragment Shader
@@ -111,6 +119,8 @@ void CompileShaders()
 		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
 		printf("Error validating program: '%s'\n", eLog);
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -170,11 +180,26 @@ int main()
 		// Get + Handle user input events
 		glfwPollEvents();
 
+		if (direction) {
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxoffset) 
+		{
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
