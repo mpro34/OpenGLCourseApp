1da4d5a583cfb5fb71ac244386748a26026807d9
Chris Whiting <mpro34@gmail.com>
2019-12-21 09:10:36 -0600

    Add projection matrix to vertex shader.

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -13,7 +13,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VBO, VAO, IBO, shader, uniformModel;
+GLuint VBO, VAO, IBO, shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -28,6 +28,7 @@ float maxSize = 0.8f;
 float minSize = 0.1f;
 
 // Vertex Shader code
+// TODO: Understand matrix math with projection matricies!
 static const char* vShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
@@ -36,10 +37,11 @@ layout (location = 0) in vec3 pos;											  \n\
 out vec4 vCol;																  \n\
                                                                               \n\
 uniform mat4 model;                                                           \n\
+uniform mat4 projection;                                                      \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = model * vec4(pos, 1.0);									  \n\
+    gl_Position = projection * model * vec4(pos, 1.0);						  \n\
 	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0);								  \n\
 }";
 
@@ -158,6 +160,7 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main()
@@ -214,6 +217,8 @@ int main()
 	CreateTriangle();
 	CompileShaders();
 
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
+
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
@@ -260,15 +265,13 @@ int main()
 
 		glm::mat4 model(1.0f);
 
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, -2.5f));
+		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 
-
-
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
