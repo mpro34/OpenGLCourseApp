fcdb0e329d72bbf33c264ad279d9357a75d6d69b
Chris Whiting <mpro34@gmail.com>
2019-12-25 03:03:54 -0600

    Create mouse and key press registers

--- OpenGLCourseApp/Window.h
+++ OpenGLCourseApp/Window.h
@@ -19,6 +19,11 @@ public:
 
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
 
+	bool* getKeys() { return keys;  }
+
+	GLfloat getXChange();
+	GLfloat getYChange();
+
 	void swapBuffers() { glfwSwapBuffers(mainWindow); }
 
 	~Window();
@@ -28,4 +33,16 @@ private:
 
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024];
+
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	bool mouseFirstMoved;
+
+	void createCallbacks();
+	static void handleKeys(GLFWwindow *window, int key, int code, int action, int mode); // Used for callback functionality
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
